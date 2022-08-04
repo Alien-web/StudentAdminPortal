a78487e6d0a77d9e9d2f1b979bcf55ffc22d4213
Alien-Web <surumypallickal@gmail.com>
2022-08-04 13:06:00 +0530

    End of section 3

---
+++ studentadminportal-ui/src/app/students/student.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Student } from '../Models/API-Models/student.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+  baseUrl='https://localhost:44370/';
+  constructor(private _http:HttpClient) { }
+
+  getStudents():Observable<Student[]>{
+    return this._http.get<Student[]>(this.baseUrl+'students');
+  }
+}
---
+++ studentadminportal-ui/src/app/students/students.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Student } from '../Models/UI-Models/student.model';
+import { StudentService } from './student.service';
+
+@Component({
+  selector: 'app-students',
+  templateUrl: './students.component.html',
+  styleUrls: ['./students.component.css']
+})
+export class StudentsComponent implements OnInit {
+  students:Student[]=[];
+  filterString='';
+  displayedColumns: string[] = ['firstName', 'lastName', 'dateOfBirth','email', 'mobile', 'address', 'gender','profileImageUrl'];
+  dataSource:MatTableDataSource<Student>=new MatTableDataSource<Student>();
+  @ViewChild(MatPaginator) matPaginator!:MatPaginator;
+  @ViewChild(MatSort) matSort!:MatSort;
+
+  constructor(private _studentService:StudentService) { }
+
+  ngOnInit() {
+    this._studentService.getStudents().subscribe(res=>{
+      console.log('success',res)
+      this.students=res;
+      this.dataSource=new MatTableDataSource<Student>(this.students);
+      if(this.matPaginator){
+        this.dataSource.paginator=this.matPaginator;
+      }
+      if(this.matSort){
+        this.dataSource.sort=this.matSort;
+      }
+    },error=>{
+      console.log('eror',error);
+    })
+  }
+
+  filerStudents(event:any){
+    console.log(event);
+    this.dataSource.filter=this.filterString.trim();
+  }
+
+}
